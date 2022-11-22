29bd4077eeb46c123a8e143e7995bc1ac77281b1
LAR9813 <lar9813@gmail.com>
2022-11-21 23:39:29 -0500

    Pet Store Website - Libraries import, files configuration and homepage creation

---
+++ src/main/java/petstore/controllers/ControllerHomePage.java
@@ -0,0 +1,14 @@
+package petstore.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class ControllerHomePage {
+
+	@RequestMapping
+	public String homePage() {
+
+		return "homePage";
+	}
+}
